60783e8bb3caf56d5d9f66c81a033e7064b1bc11
code043 <procoder043@gmail.com>
2026-05-07 22:30:24 -0300

    chore: auth and user modules

---
+++ src/modules/auth/dto/create-auth.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsEmail()
+  email!: string;
+  @IsString()
+  @MinLength(6)
+  password!: string;
+}
