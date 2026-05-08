bc23991907b3129e3e5f66485ff2767f84f23613
code043 <procoder043@gmail.com>
2026-05-08 00:02:55 -0300

    refactor: hashing password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,20 +1,26 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  private salt = 10;
   constructor(private prisma: PrismaService) {}
   async register(createAuthDto: CreateAuthDto) {
-    const { email } = createAuthDto;
+    const { password, email } = createAuthDto;
     const existingUser = await this.prisma.user.findUnique({
       where: { email },
     });
     if (existingUser) {
       throw new ConflictException('Email already existis!');
     }
+    const hashedPassword = bcrypt.hashSync(password, this.salt);
     return await this.prisma.user.create({
-      data: createAuthDto,
+      data: {
+        ...createAuthDto,
+        password: hashedPassword,
+      },
     });
   }
 
